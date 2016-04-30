da9e2d992a286701c768c5fe0140d5cfc11306ea
addisonn <addisonzxy@gmail.com>
2016-04-30 11:18:17 -0700

    adding more stuff

---
+++ quiz.css
@@ -0,0 +1,26 @@
+var tCorrect = 0;
+var tContent = 1;
+function tCheck()
+{
+	if(tContent == 1)
+  {
+    var who = $('input[name="radio-choice-0"]:checked').attr('id');
+    //window.alert(who);
+    if(who == "radio-choice-0a")
+    {
+    	$("#tAnswer").text("AWWW...Nope, it's actually Kanye who said it.");
+      $("#check").hide();
+  		$("#next").show();
+    }
+    else if(who == "radio-choice-0b")
+    {
+    	$("#tAnswer").text("Yup! Kanye said it, good job!");
+      $("#check").hide();
+  		$("#next").show();
+    }
+    else
+    {
+    	$("#tAnswer").text("You have to pick something before checking the answer-_-");
+    }
+  }
+}
