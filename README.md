b13ec7e3e9e79f151e238c0141acf8e5afcfcf2e
addisonn <addisonzxy@gmail.com>
2016-05-07 10:57:09 -0700

    working on stuff

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # gwcFinalProject1
-My first girls who code final project
+This is going to me me and Lucinda's first Girls who Code final project! We are going to make some interesting quzzies on a website that we designed using HTML, CSS, Javascript, and jQuery Mobile. It's going to be awesome!
--- quiz.js
+++ quiz.js
@@ -1,26 +1,46 @@
+
+
+$(document).on("pageshow","#TvsKQuiz",function(){
+  //alert("pageshow event fired - pagetwo is now shown");
+  $("#next").hide();
+});
 var tCorrect = 0;
 var tContent = 1;
+var kCorrect = "Yup! Kanye said it, good job!";
+var dWrong = "AWWW...Nope, it's actually Kanye who said it.";
 function tCheck()
 {
+	var who = $('input[name="radio-choice-0"]:checked').attr('id');
+	if(who == undefined)
+	{
+		$("#tAnswer").text("You have to pick something before checking the answer-_-");
+	}
+	else
+	{
+		if(tContent == 1)
+		{
+			//window.alert(who);
+			if(who == "radio-choice-0a")
+			{
+				$("#tAnswer").text(dWrong);
+			  
+			}
+			else if(who == "radio-choice-0b")
+			{
+				$("#tAnswer").text(kCorrect);
+			  
+			}
+		}
+		$("#check").hide();
+		$("#next").show();
+		//tContent ++;
+	}	
+}
+
+function tNext()
+{
 	if(tContent == 1)
-  {
-    var who = $('input[name="radio-choice-0"]:checked').attr('id');
-    //window.alert(who);
-    if(who == "radio-choice-0a")
-    {
-    	$("#tAnswer").text("AWWW...Nope, it's actually Kanye who said it.");
-      $("#check").hide();
-  		$("#next").show();
-    }
-    else if(who == "radio-choice-0b")
-    {
-    	$("#tAnswer").text("Yup! Kanye said it, good job!");
-      $("#check").hide();
-  		$("#next").show();
-    }
-    else
-    {
-    	$("#tAnswer").text("You have to pick something before checking the answer-_-");
-    }
-  }
+	{
+		
+	}
 }
